d30942dd3735bad6f696b789937a3eeee289e189
obiohaa <obiohaebuka200215@gmail.com>
2022-09-28 20:55:21 +0100

    added item after IP insert

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://ae499f8edbd464e13a69ceaed8e65bdc-1491622887.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
