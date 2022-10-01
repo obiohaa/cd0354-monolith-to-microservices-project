d5c69fd70ea07b73f3a588546b92dda7a1911dc6
obiohaa <obiohaebuka200215@gmail.com>
2022-10-01 18:54:50 +0100

    saturday oct one

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://ab847547c64744dc4837a163fee55636-181080778.us-east-1.elb.amazonaws.com:8080/api/v0'
+  apiHost: 'http://localhost:8080/api/v0'
 };
 
 /*
